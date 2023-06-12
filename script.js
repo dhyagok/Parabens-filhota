172a3442ad199cdb924faf49f3557fe04d1e2068
dyego <dyego.k@hotmail.com>
2023-06-12 17:11:59 -0300

    novo site

---
+++ script.js
@@ -0,0 +1,13 @@
+let contador = 1;
+
+setInterval(function(){
+
+    document.getElementById('slide' + contador).checked = true;
+    contador ++;
+
+    if (contador > 32) {
+        contador = 1;
+    }
+
+}, 2000)
+
